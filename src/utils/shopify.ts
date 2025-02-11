9bd9dfb8df2c025260e883918f906a374f2f1412
lazaralex98 <alex2012ro@gmail.com>
2025-02-11 22:46:11 +0200

    increase limit of products to 50

--- src/utils/shopify.ts
+++ src/utils/shopify.ts
@@ -73,7 +73,7 @@ export const getProducts = async (options: {
   buyerIP: string;
   collectionId: string; // Add collectionId option
 }) => {
-  const { limit = 30, buyerIP, collectionId } = options;
+  const { limit = 50, buyerIP, collectionId } = options;
 
   const data = await makeShopifyRequest(
     ProductsQuery,
