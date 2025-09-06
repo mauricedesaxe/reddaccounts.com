0c4f7dfc2690c70365cca40d421e0a40e455b5df
lazaralex98 <alex@leetsoftware.com>
2025-09-06 21:01:09 +0300

    use cloudflare adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,5 @@
 import { defineConfig } from "astro/config";
-import node from "@astrojs/node";
+import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 import tailwind from "@astrojs/tailwind";
@@ -10,9 +10,7 @@ import svelte from "@astrojs/svelte";
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: node({
-    mode: "standalone",
-  }),
+  adapter: cloudflare(),
   integrations: [
     tailwind({
       config: {
